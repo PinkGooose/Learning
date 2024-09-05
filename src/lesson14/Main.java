ad904c02eee2be0b7261f19f66c76333972c166a
EvgeniaRudenko <evgeniarudenko2005@gmail.com>
2024-09-05 20:27:45 +0300

    Исправлен баг при регистрации!

--- src/lesson14/Main.java
+++ src/lesson14/Main.java
@@ -3,6 +3,8 @@ package lesson14;
 public class Main {
     public static void main(String[] args) {
         TaskExecutor executor = new TaskExecutor();
-        executor.execute();
+        executor.execute(); //валрлдавдлыржла
+
+        System.out.println("Привет Мир!");
     }
 }
